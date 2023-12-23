bfeefde078075763f5e54ca049fae1db04570d94
VlaSerega <sergey.lipps@gmail.com>
2023-12-23 10:30:38 +0700

    Fix message url

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('api.conversations.urls')),
+    path('', include('api.messages.urls')),
     path('', include('api.users.urls'))
 ]
