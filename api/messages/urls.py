824c06d2ec2523b5ced78f9cd04bc7602696449b
VlaSerega <sergey.lipps@gmail.com>
2023-12-23 10:31:50 +0700

    Fix message url

--- api/messages/urls.py
+++ api/messages/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from api.messages.views import MessageViewSet
 
 router = DefaultRouter()
-router.register("conversations", MessageViewSet)
+router.register("messages", MessageViewSet)
 
 urlpatterns = router.urls
