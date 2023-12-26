92ff48e95a3c55d892345036978c5e2561172d7f
VlaSerega <sergey.lipps@gmail.com>
2023-12-26 11:56:17 +0700

    Add model to admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from api.models import *
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Conversation)
+admin.site.register(Message)
+admin.site.register(ConversationParticipants)
+admin.site.register(Contacts)
